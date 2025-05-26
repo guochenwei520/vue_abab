8331adf62f4518b07ef9c84b8148fe4f99dd9e49
guochenwei520 <2634549599@qq.com>
2025-05-26 13:17:25 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,24 +4,19 @@ import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [vue()],
+// })
+
 export default defineConfig({
-    plugins: [
-        vue(),
-        vueDevTools(),
-    ],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        },
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     },
-    server: {
-        proxy: {
-            '/api': {
-                target: 'http://localhost:80/',
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, ''),
-            },
-        }
-    }
+  },
 })
